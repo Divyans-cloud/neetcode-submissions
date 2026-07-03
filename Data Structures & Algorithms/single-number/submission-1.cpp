cf5cfecd4e3257ed898c88ecdede560c71d6a34f
DIVYANS JANGID <divyansramnivasjangid@gmail.com>
2026-07-03 22:55:53 +0530

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<long long,long long>frequency;
+        for(int i=0;i<nums.size();i++){
+            frequency[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(frequency[nums[i]]==1) 
+            return nums[i];
+        }
+        return -1;
+        
+    }
+};
